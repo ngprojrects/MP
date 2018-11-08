ac22421d966130343198886d4a95d32926353e85
Shabbeer <shabbeer@zen.com.my>
2018-11-08 19:11:25 +0530

    phone number mask, state selection

--- src/app/mp-partners/mp-partners-form/mp-partners-form.component.ts
+++ src/app/mp-partners/mp-partners-form/mp-partners-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Partner } from 'src/app/models/partner';
+import { GetInboxService } from 'src/app/services/get-inbox.service';
 
 @Component({
   selector: 'app-mp-partners-form',
@@ -9,18 +10,25 @@ import { Partner } from 'src/app/models/partner';
 export class MpPartnersFormComponent implements OnInit {
 
   brands = ['Select Brand', 'BT&T', 'CRds', 'GTm'];
-  states = ['Select State', 'Waticon', 'Coim', 'Cegar'];
+  states = ['Select State'];
   countries = ['Select Country', 'US', 'UK', 'London'];
   partnerValues = { name: '', brand: this.brands[0], streetAddress: '', city: '', country: this.countries[0], state: this.states[0], zip: '', phone: '', url: '' };
 
   namePattern = '^[a-z0-9_-]{8,15}$';
   postalCodePattern = '^(\d{5}(-\d{4})?|[A-Z]\d[A-Z] *\d[A-Z]\d)$';
-  // phonePattern = '\d{3}[\-]\d{3}[\-]\d{4}';
+  phonePattern = "\(\d{3}\)\d{3}\-\d{4}";
+  phoneMask = ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
   urlPattern = '^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$'
-  constructor() {
+  constructor(public getApi: GetInboxService) {
 
   }
-  ngOnInit() { }
+  ngOnInit() {
+    this.getApi.getApiModel('assets/countryStates.json').subscribe(res => {
+      res.forEach(element => {
+        this.states.push(element.name)
+      });
+    })
+  }
 
   onSubmit(formValues: any) {
 
--- src/app/mp-partners/mp-partners-form/mp-partners-form.module.ts
+++ src/app/mp-partners/mp-partners-form/mp-partners-form.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MpPartnersFormComponent } from './mp-partners-form.component';
 import { MpPartnersFormRoutingModule } from './mp-partners-form-routing.module';
-
+import { TextMaskModule } from 'angular2-text-mask';
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
         ReactiveFormsModule,
         MpPartnersFormRoutingModule,
-
+        TextMaskModule
     ],
     declarations: [
         MpPartnersFormComponent,
